8e013aa826162abeba51262c1ed337f8ade84efb
Nathaniel Owusu <owusun2002@gmail.com>
2024-11-03 23:11:05 -0600

    firebase setup

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collectoin, getDocs} from 'firebase/firestore/lite'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCF7VJ4sTNCrs19LVRiWRCPjNk9PeXm2WA",
+  authDomain: "soundscout-1591b.firebaseapp.com",
+  projectId: "soundscout-1591b",
+  storageBucket: "soundscout-1591b.firebasestorage.app",
+  messagingSenderId: "631492086503",
+  appId: "1:631492086503:web:7f9180a99dc115d006b042",
+  measurementId: "G-YS3LN6DR7W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
